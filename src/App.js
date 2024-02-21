b08f7cb2fb874a22e9dd4acda8df4b07ae4e99c3
Rokkam Anil <anilrokkam91@gmail.com>
2024-02-21 12:08:40 +0530

    Modified Navbar for User

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import CustomerRouters from './Pages/Router/CustomerRouters';
 import AdminRouters from './Pages/Router/AdminRouters';
 import { Aboutpage } from './Pages/User/Aboutpage';
 import UserPage from './Pages/User/UserPage';
+import Login from './Pages/Register/Login';
+import Signup from './Pages/Register/Signup';
+import Contact from './Pages/User/components/Contact';
 
 function App() {
   return (
    <div>
-       {/* <Routes>
+       <Routes>
           <Route path='/*' element={<CustomerRouters/>} />
           <Route path='/admin/*' element={<AdminRouters/>} /> 
-       </Routes> */}
-       <Aboutpage/>
+          <Route path='/User/*' element={<UserPage/>} />
+          <Route path='/About/*' element={<Aboutpage/>} />
+          <Route path='/Register/*' element={<Login/>} />
+          <Route path='/Signup/*' element={<Signup/>} />
+          <Route path='/Contact/*' element={<Contact/>} />
+       </Routes>
+       
    </div>
   );
 }
